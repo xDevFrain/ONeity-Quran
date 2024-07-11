160c3164976c434657051b278e892fa57b27ab00
OsamaNaim85 <osamaneim85@gmail.com>
2024-07-12 01:19:05 +0300

    ONeity Quran Bot For ONeity Community Server

---
+++ README.md
@@ -0,0 +1,23 @@
+# ONeity Quran Radio
+
+## Discord bot Quran Radio 📻🥰
+
+A new Node.js project.
+
+### Getting Started
+
+This project is a starting point for a discord bot application.
+
+1- Listen fully to the various radio channels of the Quran by running the bot" القران"
+
+
+
+### Packages & technologies :
+
+<p>
+      <img alt="JavaScript" src="https://img.shields.io/badge/JavaScript-F7DF1E.svg?logo=javascript&logoColor=black">
+      <img alt="Node.js" src="https://img.shields.io/badge/Node.js-43853D.svg?logo=node.js&logoColor=white">
+ 
+</p>
+
+![License MIT](https://img.shields.io/badge/license-MIT-green.svg)
---
+++ src/ComponentsAction/setupModal/set_Radio.js
@@ -0,0 +1,53 @@
+const { ButtonStyle, ButtonBuilder, ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, ModalSubmitInteraction, ChannelType } = require("discord.js");
+
+/**
+ * @type {import("@utils/types/baseComponent")}
+ */
+module.exports = {
+  name: "Modal_set_RadioChannel",
+  enabled: true,
+  /**
+   * @param {ModalSubmitInteraction} interaction 
+   */
+  async action(client, interaction, parts, lang) {
+    try {
+      let channelId = interaction.fields.getTextInputValue("RadioChannel").trim()
+
+      let channel = await interaction.guild.channels.fetch(channelId).catch(e => null) || interaction.guild.channels.cache.find(ch => ch.name === channelId)
+      if (!channel) return interaction.reply({
+        content: "❌ | لم يتم العثور علي القناه الصوتيه",
+        ephemeral: true
+      })
+      let data = await client.db.table("channels").get(`${interaction.guildId}_radioChannel`)
+
+      if (channel.type === ChannelType.GuildVoice) {
+
+        let isChannel = await client.db.table("channels").get(`${interaction.guildId}_radioChannel`)
+
+        if (isChannel?.channelId === channel.id) return interaction.reply({
+          content: ":warning: | هذه القناه تم تعينها بالفعل من قبل ",
+          ephemeral: true
+        })
+
+        await client.db.table("channels").set(`${interaction.guildId}_radioChannel`, {
+          channelId: channel.id,
+          guildId: interaction.guildId,
+          url: data?.url || "http://n02.radiojar.com/v33ay8543d0uv?rj-ttl=5&rj-tok=AAABgDTqH90AIyBNaL5t4qE1IA",
+          enabled: false
+        })
+
+        interaction.reply({
+          content: `✅ | تم تعين القناه الصوتيه ${channel}\n يمكنك التشغيل و الايقاف  وتغير الاذاعة عبر استخدام امر /control حتي يمكنك التحكم به`,
+          ephemeral: true
+        })
+
+      } else interaction.reply({
+        content: `🛑 | هذه ليست قناه صوتيه ${channel} من فضلك قم بتعين قناه صوتيه بشكل صحيح`,
+        ephemeral: true
+      })
+
+    } catch (err) {
+      console.log(err)
+    }
+  },
+};
